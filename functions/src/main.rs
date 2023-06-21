14e38241459f003d78bcfe7dddd7c3c79d290c1c
DanMyers300 <contact+github@danmyers.net>
2023-06-21 09:52:07 -0500

    learning return values

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,27 @@
+// Purpose: Learning Rust
+
+fn main() {
+    let x = plus_one(5);
+
+    println!("The value of x is: {x}");
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
+/*
+fn five() -> i32 {
+    5
+}
+
+fn main() {
+    let x = five();
+
+    println!("The value of x is: {x}");
+}
+
+
 fn main() {
     //!print_labeled_measurement(5, 'h');
     let y = {
@@ -8,7 +32,7 @@ fn main() {
     println!("The value of y is: {}", y);
 }
 
-/*
+
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
 }
